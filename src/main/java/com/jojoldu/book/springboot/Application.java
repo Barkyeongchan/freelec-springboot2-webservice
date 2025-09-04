da637a8f221756e27d37869a77be6a6be3a54ffe
Barkyeongchan <xodudcksxo@gmail.com>
2025-09-04 14:47:29 +0900

    오전 수업 진행

---
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// 스프링 부트의 자동 설정, 스프링 brean 읽기와 생성을 모두 자동으로 합니다.
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+
+    }
+}
