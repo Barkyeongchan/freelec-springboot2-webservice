2f8adc75c777cbec730cbc530a75684d9946422f
Barkyeongchan <xodudcksxo@gmail.com>
2025-09-05 17:24:50 +0900

    9월 5일 수업 진행

--- src/test/java/com/jojoldu/book/springboot/domain/posts/postsRepositoryTest.java
+++ src/test/java/com/jojoldu/book/springboot/domain/posts/postsRepositoryTest.java
@@ -16,7 +16,7 @@ public class PostsRepositoryTest {
     @Autowired
     PostsRepository postsRepository;
 
-    // JUnit에서 단위 테스트가 끝날 때마다 수행되는 메소드 지정
+    //Junit에서 단위 테스트가 끝날 때마다 수행되는 메소드를 지정
     @AfterEach
     public void cleanup() {
         postsRepository.deleteAll();
@@ -28,7 +28,7 @@ public class PostsRepositoryTest {
         String title = "테스트 게시글";
         String content = "테스트 본문";
 
-        // 테이블 posts에 insert/update 쿼리를 실행
+        // 테이블 posts에 insert/update 쿼리를 실행합니다
         postsRepository.save(Posts.builder()
                 .title(title)
                 .content(content)
@@ -47,14 +47,14 @@ public class PostsRepositoryTest {
     @Test
     public void BaseTimeEntity_등록() {
         //given
-        LocalDateTime now = LocalDateTime.of(2019, 6, 4, 0, 0, 0);
+        LocalDateTime now = LocalDateTime.of(2025, 9, 5, 3, 12, 0);
         postsRepository.save(Posts.builder()
                 .title("title")
                 .content("content")
                 .author("author")
                 .build());
         //when
-        // 테이블 posts에 있는 모든 데이터를 조회해오는 메소드
+        // 테이블 posts에 있는 모든 데이터를 조회해오는 메소드입니다.
         List<Posts> postsList = postsRepository.findAll();
 
         //then
